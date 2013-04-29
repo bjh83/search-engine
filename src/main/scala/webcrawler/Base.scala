8c0cab146f2a139d30150deff329f538253ece5d
Brendan Higgins <bjh83@case.edu>
2013-04-29 02:52:05 -0400

    I added Index

--- src/main/scala/webcrawler/Base.scala
+++ src/main/scala/webcrawler/Base.scala
@@ -10,3 +10,12 @@ object Pages extends Table[(String, Long)]("PAGES") {
   def * = url ~ lastVisited
 }
 
+object Index extends Table[(String, String, Int)]("INDEX") {
+  def url = column[String]("PAGE_URL")
+  def word = column[String]("INDEX_WORD")
+  def count = column[Int]("INDEX_COUNT")
+
+  def * = url ~ word ~ count
+  def page = foreignKey("PAGE_FK", url, Pages)(_.url)
+}
+
